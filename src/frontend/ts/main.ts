94a425a5b3955f6e3954fab589db72fb8602b1a3
juan-ignacio-yunes <jignacio.yl@gmail.com>
2023-06-26 17:59:37 -0300

    se modificaron indentaciones

--- src/frontend/ts/main.ts
+++ src/frontend/ts/main.ts
@@ -18,38 +18,36 @@ class Main implements EventListenerObject,HttpResponse {
     }
     manejarRespueta(respueta: string) {
         var lista: Array<Device> = JSON.parse(respueta);
-
-        
         var ulDisp = document.getElementById("listaDisp");
+        
         for (var disp of lista) {
             var item: string = `<li class="collection-item avatar">`;
-                    if(disp.type==1){
-                      item+=  '<img src="static/images/lightbulb.png" alt = "" class="circle" >'
-                    } else {
-                        item+=  '<img src="static/images/window.png" alt = "" class="circle" >'
-                    }
-                          
-                        item+=`<span class="titulo">${disp.name}</span>
-                          <p>
-                          ${disp.description}
-                          </p>
-                          <a href="#!" class="secondary-content">
-                          <div class="switch">
-                          <label>
-                            Off
-                            `;
-                            if (disp.state) {
-                                item +=`<input type="checkbox" checked id="ck_${disp.id}">`;
-                            } else {
-                                item +=`<input type="checkbox" id="ck_${disp.id}" >`;
-                            }
-                            item += `
-                            <span class="lever"></span>
-                            On
-                          </label>
-                        </div>
-                          </a>
-                        </li>`;
+            if(disp.type==1){
+                item+=  '<img src="static/images/lightbulb.png" alt = "" class="circle" >'
+            } else {
+                item+=  '<img src="static/images/window.png" alt = "" class="circle" >'
+            }
+                    
+            item+=`<span class="titulo">${disp.name}</span>
+
+            <p> ${disp.description} </p>
+            <a href="#!" class="secondary-content">
+            <div class="switch">
+            <label>
+            Off
+            `;
+            if (disp.state) {
+                item +=`<input type="checkbox" checked id="ck_${disp.id}">`;
+            } else {
+                item +=`<input type="checkbox" id="ck_${disp.id}" >`;
+            }
+            item += `
+            <span class="lever"></span>
+            On
+            </label>
+            </div>
+                </a>
+            </li>`;
             
             ulDisp.innerHTML += item;
         }
